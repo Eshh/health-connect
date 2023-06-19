27779e35e6f18b16a0f8019f34983657c3c325d2
eswarprasadk <eswarprasadk@turito.com>
2023-06-19 16:42:03 +0530

    Routing changes initiated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './common/signin/signin.component';
+import { DashboardComponent } from './common/dashboard/dashboard.component';
+import { SignupComponent } from './common/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: 'sign-in',
+    component: SigninComponent,
+  },
+  {
+    path: 'sign-up',
+    component: SignupComponent,
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
