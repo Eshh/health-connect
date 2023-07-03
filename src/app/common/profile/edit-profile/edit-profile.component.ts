3877e01c79ac9c14df813ca0a175a56fc7372452
rameshd <rameshd@turito.com>
2023-07-03 11:49:46 +0530

    profile , edit profile, hospitals list pages are designed

--- src/app/common/profile/edit-profile/edit-profile.component.ts
+++ src/app/common/profile/edit-profile/edit-profile.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-edit-profile',
   templateUrl: './edit-profile.component.html',
-  styleUrls: ['./edit-profile.component.css']
+  styleUrls: ['./edit-profile.component.css',]
 })
 export class EditProfileComponent implements OnInit {
 
